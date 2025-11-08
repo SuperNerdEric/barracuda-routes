58bc47c4280146bacb321da315348ebbf709f4a8
Eric White <eric.white.austin@gmail.com>
2025-11-07 22:03:01 -0600

    Put config in section.

--- src/main/java/com/barracudaroutes/BarracudaRoutesConfig.java
+++ src/main/java/com/barracudaroutes/BarracudaRoutesConfig.java
@@ -3,14 +3,24 @@ package com.barracudaroutes;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("barracudaroutes")
 public interface BarracudaRoutesConfig extends Config
 {
+	@ConfigSection(
+		position = 0,
+		name = "Route visibility",
+		description = "Settings for automatic route tile visibility"
+	)
+	String routeVisibilitySection = "routeVisibilitySection";
+
 	@ConfigItem(
 		keyName = "hideDistance",
 		name = "Hide Distance",
-		description = "Distance in tiles at which route tiles will hide when the player is near them"
+		description = "Distance in tiles at which route tiles will hide when the player is near them",
+		section = routeVisibilitySection,
+		position = 1
 	)
 	default int hideDistance()
 	{
@@ -20,7 +30,9 @@ public interface BarracudaRoutesConfig extends Config
 	@ConfigItem(
 		keyName = "hideDelayTicks",
 		name = "Hide Delay (Ticks)",
-		description = "Number of game ticks to wait before hiding a tile after the player is near it (1 tick = 0.6 seconds)"
+		description = "Number of game ticks to wait before hiding a tile after the player is near it (1 tick = 0.6 seconds)",
+		section = routeVisibilitySection,
+		position = 2
 	)
 	default int hideDelayTicks()
 	{
@@ -30,7 +42,9 @@ public interface BarracudaRoutesConfig extends Config
 	@ConfigItem(
 		keyName = "maxVisibleTiles",
 		name = "Max Visible Tiles",
-		description = "Maximum number of route tiles visible at a time"
+		description = "Maximum number of route tiles visible at a time",
+		section = routeVisibilitySection,
+		position = 3
 	)
 	default int maxVisibleTiles()
 	{
